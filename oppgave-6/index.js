32620d484b5b371101243e30e3c45c1d95e58508
Kiriza Frederique Theosophe <54266892+kirizat-theosophe@users.noreply.github.com>
2022-10-06 21:36:52 +0200

    Add files via upload

---
+++ oppgave-6/index.js
@@ -0,0 +1,13 @@
+
+
+
+const kjendis = "Marilyn Monroe";
+let ektefelle = "Bill Gates"; 
+
+ektefelle = "Elvis";
+
+console.log(ektefelle);
+console.warn("Advarsel");
+console.info("Elvis is not rich man");
+console.error("she is beautiful lady");
+console.alert("Denne siden bruker JavaScript");
